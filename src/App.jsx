b78bbebce10f93c6489acccb9e91bc595d96bcde
Mariq12 <fmlabanda@utpl.edu.ec>
2024-06-05 10:34:13 -0500

    Se agrega degradado al fondo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import styled from "styled-components"
 
+const FondoGradiente = styled.div`
+background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+width: 100%;
+min-height: 100vh;
+`
 
 function App() {
 
   return (
     <>
-      
+      <FondoGradiente>
+        <h1>¡Hola Mundo!</h1>
+      </FondoGradiente>
     </>
   )
 }
