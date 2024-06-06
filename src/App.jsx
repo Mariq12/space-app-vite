b57174cdfb249707da694755b0da45efc557b741
Mariq12 <fmlabanda@utpl.edu.ec>
2024-06-06 12:48:55 -0500

    Se crea la galeria, se conecta con el App y se agrega estilos

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Headboard from "./components/headboard/Headboard"
 import Sidebar from "./components/sidebar/Sidebar"
 import Banner from "./components/banner/Banner"
 import bannner from "./assets/banner.png"
+import Gallery from "./components/gallery/Gallery"
 
 const FondoGradiente = styled.div`
 background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -19,18 +20,26 @@ const MainContainer = styled.div`
   display: flex;
   gap: 24px;
 `
+const GalleryContent = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
+
 function App() {
 
   return (
     <>
       <FondoGradiente>
         <GlobalStyles />
-
         <AppContainer>
           <Headboard />
           <MainContainer>
             <Sidebar />
-            <Banner texto="¡Bienvenidos a la galería más completa de fotos!" backgroundImage={bannner} />
+            <GalleryContent>
+              <Banner texto="¡Bienvenidos a la galería más completa de fotos!" backgroundImage={bannner} />
+              <Gallery />
+            </GalleryContent>
           </MainContainer>
         </AppContainer>
       </FondoGradiente>
---
+++ src/components/gallery/Gallery.jsx
@@ -0,0 +1,11 @@
+
+
+const Gallery = () => {
+  return (
+    <h2>
+        Navegue por la galería
+    </h2>
+  );
+};
+
+export default Gallery;
