eb1afa6daf8f78eee6f38a1bd95c6496e272f5f7
Mariq12 <fmlabanda@utpl.edu.ec>
2024-06-05 22:26:02 -0500

    Instalando fuentes y esttilizando el item de navegación

--- src/components/sidebar/itemNavigation/ItemNavigation.jsx
+++ src/components/sidebar/itemNavigation/ItemNavigation.jsx
@@ -2,12 +2,18 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const StyledItemList = styled.li`
-
+    font-size: 24px;
+    margin-bottom: 30px;
+    line-height: 28px;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    color: ${props => props.$active ? "#7B78E5" : "#D9D9D9"};
 `
 
 const ItemNavigation = ({children, activeIcon, idleIcon, active=false}) => {
     return (
-        <StyledItemList> 
+        <StyledItemList $active={active}> 
         <img src={active ? activeIcon : idleIcon} />
         {children}
         </StyledItemList>
