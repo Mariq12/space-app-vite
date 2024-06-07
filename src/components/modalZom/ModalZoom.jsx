bf77192aa710b372ec4c79192c43e1a0c190d4f0
Mariq12 <fmlabanda@utpl.edu.ec>
2024-06-07 13:59:45 -0500

    Añadiendo opacidad al fondo

--- src/components/modalZom/ModalZoom.jsx
+++ src/components/modalZom/ModalZoom.jsx
@@ -1,15 +1,32 @@
 import PropTypes from 'prop-types';
 import Image from '../gallery/image/Image';
+import styled from 'styled-components';
+
+const Overlay= styled.div`
+    background-color: rgba(0, 0, 0, .7);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+`
+const StyledDialogue = styled.dialog`
+    position: absolute;
+    top: 294px;
+`
 
 const ModalZoom = ({ photo }) => {
     return (
         <>
-            { photo && <dialog open={!!photo}>
+            { photo && <>
+                <Overlay />
+                < StyledDialogue open={!!photo}>
                     <Image photo={photo} expandida={true} />
                     <form method="dialog">
                         <button>OK</button>
                     </form>
-                </dialog>
+                </ StyledDialogue>
+            </>
             }
         </>
     )
